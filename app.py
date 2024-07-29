00249a48560a7074e0f9e97dea5c858b21abbc91
unknown <nikita.gordynskii@gmail.com>
2024-07-29 21:14:43 +0200

    First things testing like login input pass input and click on login button

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
+import creds
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.facebook.com/")
 
 title = driver.title
-print(title)
 assert title == "Facebook – zaloguj się lub zarejestruj"
 
-
-
-# time.sleep(20)
+driver.find_element(By.NAME, "email").clear()
+driver.find_element(By.NAME, "email").send_keys(creds.creds()[0])
+driver.find_element(By.NAME, "pass").clear()
+driver.find_element(By.NAME, "pass").send_keys(creds.creds()[1])
+print(driver.find_element(By.NAME, "login"))
